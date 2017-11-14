4c64903cc1896cc0e4295ec922fe07648c84e23f
Ed Elliott <goeddieuk@gmail.com>
2017-11-14 22:32:28 +0000

    making actually work!

--- src/PestWatch.psm1
+++ src/PestWatch.psm1
@@ -2,10 +2,11 @@ Import-Module -Name Pester
 
 class ChangeSerializer {
     static [boolean]$listening = $false;
-    static [boolean]$signalled = $false;
+    static [boolean]$signalled = $true;
     
     static [int]$WatchInterval;
     static $PesterArguments;
+    static [IO.FileSystemWatcher]$Instance;
 
     static WaitForSignal() {
         
@@ -13,16 +14,19 @@ class ChangeSerializer {
         while ($true) {            
         
             if (![ChangeSerializer]::signalled) {
-                
-                Start-Sleep -Seconds "$([ChangeSerializer]::WatchInterval)"
+               
+                [int]$interval = [ChangeSerializer]::WatchInterval
+                Start-Sleep -Seconds $interval
             }
 
             if ([ChangeSerializer]::signalled) {                
+                
                 [ChangeSerializer]::signalled = $false
                 Write-Host "Invoke-Pester at $(Get-Date)"
                 $pesterArgs = [ChangeSerializer]::PesterArguments
 
                 Invoke-Pester @pesterArgs
+                Write-Host "******* Invoke-Pester completed at $(Get-Date) ******* "
             }
         }
     }
@@ -50,7 +54,7 @@ This runs forever, to exit do ctrl+c
 Function Invoke-PesterWatcher {
     [CmdLetBinding()]
     Param(
-        [string]$watchFolder,
+        [string]$watchFolder = "./",
         $watchIntervalSeconds = 2,
         $Script,
         $TestName,
@@ -66,6 +70,10 @@ Function Invoke-PesterWatcher {
         $OutputFile,
         $OutputFormat
     )
+
+    $watchFolder = (Get-Item $watchFolder).FullName
+    Write-Host "Watching Folder: `"$watchFolder`""
+
     #Original from https://gallery.technet.microsoft.com/scriptcenter/Powershell-FileSystemWatche-dfd7084b
     #By BigTeddy 05 September 2011 
         
@@ -75,11 +83,18 @@ Function Invoke-PesterWatcher {
 
     [ChangeSerializer]::WatchInterval = $watchIntervalSeconds
     
+    
     $filter = '*.ps*1'  # You can enter a wildcard filter here. 
     
-   
+        if([ChangeSerializer]::Instance -ne $null){
+            [ChangeSerializer]::listening = $false
+            [ChangeSerializer]::Instance.Dispose();
+            Get-Job | Stop-Job
+            Get-Job | Remove-Job
+            Get-EventSubscriber -Force | Unregister-Event -Force
+        }
         # In the following line, you can change 'IncludeSubdirectories to $true if required.                           
-        $fsw = New-Object IO.FileSystemWatcher $watchFolder, $filter -Property @{IncludeSubdirectories = $false; NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'} 
+        $fsw = New-Object IO.FileSystemWatcher $watchFolder, $filter -Property @{IncludeSubdirectories = $true; NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'} 
         
         Register-ObjectEvent $fsw Created -SourceIdentifier FileCreated -Action { 
         
@@ -89,10 +104,12 @@ Function Invoke-PesterWatcher {
             
 
         Register-ObjectEvent $fsw Changed -SourceIdentifier FileChanged -Action { 
-        
+            
             [ChangeSerializer]::signalled = $true
         
-        }  | Out-Null
+        } | Out-Null
+
+        [ChangeSerializer]::Instance = $fsw
 
 
         if (![ChangeSerializer]::listening) {
@@ -106,7 +123,3 @@ Function Invoke-PesterWatcher {
 
 
 Export-ModuleMember -Function Invoke-PesterWatcher
-# To stop the monitoring, run the following commands: 
-# Unregister-Event FileDeleted 
-# Unregister-Event FileCreated 
-# Unregister-Event FileChanged
